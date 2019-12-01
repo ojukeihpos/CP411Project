cb386d32b53b96c69dfb11d239e0c4b2e31be32d
ojukeihpos <sophie@tetrisconcept.net>
2019-11-30 19:50:47 -0500

    Ready for initial submission

--- A4.js
+++ A4.js
@@ -26,7 +26,7 @@ window.onload = function() {
     planetsDescriptions = ["The Sun", "Mercury", "Venus","Earth", "Mars", "Jupiter","Saturn", "Uranus", "Neptune" ]
     distanceFromSun = ["0.00 km","57.91 million km","108.2 million km","149.6 million km", "227.9 million km", "778.5 million km", "1.434 billion km", "2.871 billion km", "4.495 billion km"]
     mass = ["1.989 × 10^30 kg", "3.285 × 10^23 kg","4.867 × 10^24 kg", "5.972 × 10^24 kg","6.39 × 10^23 kg","1.898 × 10^27 kg","5.683 × 10^26 kg", "8.681 × 10^25 kg", "1.024 × 10^26 kg"]
-    surfaceArea = ["6.09 × 10^12 km²", "74.8 million km²"," 460.2 million km²", "510.1 million km²", "144.8 million km²", "61.42 billion km²", "42.7 billion km²", "8.083 billion km²", "7.618 billion km²"]
+    surfaceArea = ["6.09 × 10^12 km²", "74.8 million km²","460.2 million km²", "510.1 million km²", "144.8 million km²", "61.42 billion km²", "42.7 billion km²", "8.083 billion km²", "7.618 billion km²"]
     orbitalPeriod = ["0 days", "88 days","225 days", "365 days", "687 days", "12 years", "29 years", "84 years", "165 years"]
     radius = ["695,510 km","2,439.7 km","6,051.8 km", "6,371 km", "3,389.5 km", "69,911 km", "58,232 km", "25,362 km", "24,622 km"]
 
